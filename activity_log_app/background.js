64e47d40b174188f92983a2842b94db7f7291d78
Kay Schneider <kayoliver82@gmail.com>
2013-10-27 13:46:43 +0100

    loacl

--- activity_log_app/background.js
+++ activity_log_app/background.js
@@ -5,6 +5,10 @@ chrome.bluetooth.stopDiscovery(); //hack since we have no discovery timeout
 var deviceList = [];
 chrome.app.runtime.onLaunched.addListener(onLaunched);
 
+chrome.bluetooth.onConnection.addListener(function (socket) {
+    console.log("ON CONNECTION SOCKET:", socket);
+})
+
 function recordDevice(device) {
   console.log("recordDevice", device);
   //getServicesByAddress(device.address);
@@ -12,11 +16,11 @@ function recordDevice(device) {
 }
 
 function getDevice(device){
-  deviceList.push(device)
+  deviceList.push(device);
 }
 
 function devicesFound(){
-  console.log("Devices found", deviceList)
+  console.log("Devices found", deviceList);
 }
 
 
@@ -42,6 +46,14 @@ function getProfilesForDevice(device) {
 }
 function displayDeviceProfiles(profiles){
 	console.log('displayProfiles', profiles);
+        if(profiles !== undefined) {
+            console.log("DISPLAY ME");
+            //start the connection
+            
+            chrome.bluetooth.connect(profiles,function (tt) {
+                console.log(tt);
+            });
+        }
 
 }
 var connectCallback = function(socket) {
