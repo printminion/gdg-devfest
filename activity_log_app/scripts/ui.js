9de18ebbe02a570a8dda809872a1bafb647d0d04
@russenreaktor <m.kupriyanov@gmail.com>
2013-10-27 15:54:45 +0100

    updated UI

--- activity_log_app/scripts/ui.js
+++ activity_log_app/scripts/ui.js
@@ -16,16 +16,25 @@ var UI = {
     updateDevices: function(devices) {
         console.log('UI.updateDevices', devices);
         for(var i in devices) {
-            $('#devices_connected').append('<li data-address="' + devices[i].address + '"><a href="/user/messages"><span class="tab">' + devices[i].name + '</span></a>' + devices[i].address + '</li>');
+            $('#devices_connected').append('<li data-address="' + devices[i].address + '"><a href="/user/messages">' 
+                + devices[i].name + '</a> ' + devices[i].address + '</li>');
         }
     },
 
     onDeviceDiscovered: function(device) {
         console.log('UI.onDeviceDiscovered', device);
 
-        $('#devices_descovered').append('<li data-address="' + device.address + '"><a href="/user/messages"><span class="tab">' 
-            + device.name + '</span></a>' + device.address + '</li>');
-    }
+        $('#devices_descovered').append('<li data-address="' + device.address + '"><a href="/user/messages">' 
+            + device.name + '</a> ' + device.address + '</li>');
+    },
+    startScan: function() {
+        console.log('UI.startScan');
+    },
+    stopScan: function() {
+        console.log('UI.startScan');
+    },
+
+
 }
 
 
