61cd00f9dd649d45be98c53801a9435b45f10547
@russenreaktor <m.kupriyanov@gmail.com>
2013-10-27 10:53:38 +0100

    added display profiles

--- activity_log_app/background.js
+++ activity_log_app/background.js
@@ -1,11 +1,14 @@
 // Copyright (c) 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-chrome.bluetooth.stopDiscovery();
+chrome.bluetooth.stopDiscovery(); //hack since we have no discovery timeout
 var deviceList = [];
+chrome.app.runtime.onLaunched.addListener(onLaunched);
 
 function recordDevice(device) {
   console.log("recordDevice", device);
+  //getServicesByAddress(device.address);
+  getProfilesForDevice(device);
 }
 
 function getDevice(device){
@@ -16,10 +19,54 @@ function devicesFound(){
   console.log("Devices found", deviceList)
 }
 
-chrome.app.runtime.onLaunched.addListener(function() {
+
+function onLaunched() {
+  chrome.bluetooth.stopDiscovery(); //hack since we have no discovery timeout
   console.log("onLaunched:begin");
-chrome.app.window.create('window.html', {'width': 400,'height': 500});
+  chrome.app.window.create('window.html', {'width': 400,'height': 500});
   chrome.bluetooth.startDiscovery({deviceCallback: recordDevice});
   chrome.bluetooth.getDevices({deviceCallback: getDevice}, devicesFound);
-console.log("onLaunched:end");
-});
+  //chrome.bluetooth.stopDiscovery();
+  console.log("onLaunched:end");
+}
+
+function getServicesByAddress(adress) {
+	console.log('getServicesByAddress', adress);
+	chrome.bluetooth.getServices({deviceAddress: address}, function(data) {
+		console.log('getServices', data);
+	});	
+}
+function getProfilesForDevice(device) {
+	console.log('getProfilesForDevice', device);
+	chrome.bluetooth.getProfiles({device : device }, displayDeviceProfiles);	
+}
+function displayDeviceProfiles(profiles){
+	console.log('displayProfiles', profiles);
+
+}
+var connectCallback = function(socket) {
+	console.log('connectCallback', connectCallback);
+
+  if (socket) {
+    console.log('Connected!  Socket ID is: ' + socket.id + ' on service ' + socket.serviceUuid);
+
+  } else {
+    console.log('Failed to connect.');
+  }
+};
+var connectToDevice = function(result) {
+  if (chrome.runtime.lastError) {
+    console.log('Error searching for a device to connect to.');
+    return;
+  }
+  if (result.length == 0) {
+    console.log('No devices found to connect to.');
+    return;
+  }
+  for (var i in result) {
+    var device = result[i];
+    console.log('Connecting to device: ' + device.name + ' @ ' + device.address);
+    chrome.bluetooth.connect(
+      {deviceAddress: device.address, serviceUuid: kUUID}, connectCallback);
+  }
+};
